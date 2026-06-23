2d2ec1f374ab2edaee2e58611d0c8ce87e3309b3
dssivasandeep <dssivasandeep@gmail.com>
2026-06-23 12:44:43 +0530

    Added the health check api

---
+++ accountService/src/main/java/com/eventLedger/accountService/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.eventLedger.accountService.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @RequestMapping(
+            value = "/health",
+            method = RequestMethod.GET
+    )
+    public String health() {
+        return "Account Service UP";
+    }
+}
